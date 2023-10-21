7403a19461235e5e4af31b6bc53f160d79a79945
nicolasbok <nicolas.bok@student.uni-tuebingen.de>
2023-10-21 10:54:46 +0200

    add all

--- test_testing.py
+++ test_testing.py
@@ -1,9 +1,10 @@
 import unittest
 import testing
 
-def test_celsius_to_fahr(self):
-    self.assertEqual(celsius_to_fahr(32), 89.6 )
+class testtesting(unittest.TestCase):
+    def test_celsius_to_fahr(self):
+        self.assertEqual(testing.celsius_to_fahr(32), 89.6 )
 
 
 if __name__ == '__main__':
-    unittest.main
+    unittest.main()
