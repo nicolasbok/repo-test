03330732977efae75360f3f8e9de8ea910dd07d3
nicolasbok <nicolas.bok@student.uni-tuebingen.de>
2023-10-23 10:09:53 +0200

    first we commit the modified version

--- test_testing.py
+++ test_testing.py
@@ -8,3 +8,18 @@ class testtesting(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+assertEqual(a,b)
+assertNotEqual(a,b)
+assertTrue(x)
+assertFalse(x)
+assertIs(a,b)
+assertIsNot(a,b)
+assertIsNone(x)
+assertIsNotNone(x)
+assertIn(a,b)
+assertNotIn(a,b)
+assertIsInstance(a,b)
+assertNotIsInstance(a,b)
